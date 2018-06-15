fc553a593196c4cbf7569e900e52c47bf6045524
amarkwell8 <amarkwell8@gmail.com>
2018-06-15 16:46:33 -0700

    authUser updated on user selection

--- would-you-rather/src/components/Login.js
+++ would-you-rather/src/components/Login.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
+
 
 class Login extends Component {
+    handleLogin = (e) => {
+        e.preventDefault();
+    }
+    userSelected = (e) => {
+        e.preventDefault();
+        const {dispatch } = this.props;
+        dispatch(setAuthedUser(e.target.value));
+    }
     render() {
         return(
             <div>
                 Login
+                <select onChange={this.userSelected}>
+                    {this.props.possibleUsers.map((id) => (
+                        <option value={id} key={id}>{id}</option>
+                    ))}
+                </select>
             </div>
         )
     }
 }
 
-export default Login
+function mapStateToProps({ users} ) {
+    return {
+        possibleUsers: Object.keys(users)
+    }
+}
+
+export default connect(mapStateToProps)(Login)
