6e33bf231f251baf249898a29f410f832c0b7957
amarkwell8 <amarkwell8@gmail.com>
2018-06-19 15:06:25 -0700

    Added Question and Dashboard components

---
+++ would-you-rather/src/components/Question.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class Question extends Component {
+    render() {
+        const {question} = this.props;
+        <div>
+            Question
+        </div>
+    }
+}
+
+export default Question()
